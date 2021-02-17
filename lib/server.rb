9dc8a16b0da0d03e1f83c5ea189b6243fdcfd0f6
Peter Allen <peter_james_allen@hotmail.com>
2021-02-17 12:12:10 +0000

    added client

--- lib/server.rb
+++ lib/server.rb
@@ -1,26 +1,11 @@
 require 'socket'
-require_relative 'notelist'
 
 server = TCPServer.new(2345)
 
 socket = server.accept
 
-they_said = ''
-note_list = NoteList.new
-
-while they_said != 'quit' do
-
-  socket.puts "\n1. Add Note\n2. Print Note"
-  selection = socket.gets.chomp
-
-  case selection
-  when '1'
-    socket.puts "\nEnter note"
-    note_list.add(socket.gets.chomp )
-  when '2'
-    socket.puts note_list.notes
-  end
-
-end
+socket.puts "HELLO CLIENT WHAT IS YOUR NAME?"
+name = socket.gets.chomp
+socket.puts "THAT'S A LOVELY NAME #{name}"
 
 socket.close
